9aeecaea10ece352907a1f8ec7d23117579eaf09
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-22 06:33:36 +0530

    Add files via upload

---
+++ Lecture2.java
@@ -0,0 +1,7 @@
+public class Lecture1{
+    public static void main(String [] args){
+        System.out.println("Hello Java World ");
+        System.out.println("Sare Jahan Se Achacha Hindustan Hamara ");
+    
+    }
+}
