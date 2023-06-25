331e0cb5c7d7af87bd0f56ced9057385e8c5aede
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-25 21:00:21 +0530

    Update Lecture2.java

--- Lecture2.java
+++ Lecture2.java
@@ -1,6 +1,6 @@
-public class Lecture1{
+public class Lecture2{
     public static void main(String [] args){
-        System.out.println("Hello Java World ");
+        System.out.println("Hello Java World, This is Lecture 2 ");
         System.out.println("Sare Jahan Se Achacha Hindustan Hamara ");
     
     }
