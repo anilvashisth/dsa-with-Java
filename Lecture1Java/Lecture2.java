3b1e3dbee3b5370d15938433cf47d658e5fa26a0
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-27 21:36:32 +0530

    Add files via upload

---
+++ Lecture1Java/Lecture2.java
@@ -0,0 +1,9 @@
+public class Lecture2{
+    public static void main(String [] args){
+        System.out.println("Hello Java World ");
+        System.out.println("Sare Jahan Se Achacha Hindustan Hamara ");
+        System.out.println("Hello Java Learner ");
+
+    
+    }
+}
