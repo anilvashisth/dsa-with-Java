d22bce80b4d85a6c868eb96027efa9dc48e9a5e1
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-28 22:17:17 +0530

    Update Commit for Program 2

--- Program002.java
+++ Program002.java
@@ -1,6 +1,6 @@
-public class Lecture2{
+public class Program002{
     public static void main(String [] args){
-        System.out.println("Hello Java World, This is Lecture 2 ");
+        System.out.println("Hello Java World, This is Program 2 ");
         System.out.println("Sare Jahan Se Achacha Hindustan Hamara ");
     
     }
