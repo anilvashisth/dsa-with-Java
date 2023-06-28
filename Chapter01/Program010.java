16b14c71375a86345e200a72a0df0c5133b97678
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-28 22:38:12 +0530

    First Commit for Program 10

---
+++ Chapter01/Program010.java
@@ -0,0 +1,9 @@
+public class Program010{
+    public static void main(String [] args){
+        System.out.println("Hello Java World ");
+        System.out.println("\nSare Jahan Se Achacha Hindustan Hamara ");
+        System.out.println("\nHello Java Learner, This is Program 10 ");
+
+    
+    }
+}
