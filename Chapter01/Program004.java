7ebbae8a27eef2342c23dd14d25d88c44e2292c9
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-28 22:30:56 +0530

    Updated Commit Program 4

--- Chapter01/Program004.java
+++ Chapter01/Program004.java
@@ -1,7 +1,7 @@
-public class Program001{
+public class Program004{
     public static void main(String [] args){
         System.out.println("Hello World, This is Right Time To learn Java ");
-        System.out.println("Hello All ");
+        System.out.println("Hello All, this is Program 4 ");
     
     }
 }
