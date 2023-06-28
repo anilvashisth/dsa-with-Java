8c33c9609876df6a61f0635d61c6b057411616cd
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-28 22:32:09 +0530

    Updated Commit Program 5

--- Chapter01/Program005.java
+++ Chapter01/Program005.java
@@ -1,6 +1,6 @@
-public class Program002{
+public class Program005{
     public static void main(String [] args){
-        System.out.println("Hello Java World, This is Program 2 ");
+        System.out.println("Hello Java World, This is Program 5 ");
         System.out.println("Sare Jahan Se Achacha Hindustan Hamara ");
     
     }
