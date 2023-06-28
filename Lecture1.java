308c8f43918240f8310e4b2e41313004f7d6fb2a
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-28 22:15:10 +0530

    Updated Commit for Program 1

--- Lecture1.java
+++
@@ -1,7 +0,0 @@
-public class Lecture1{
-    public static void main(String [] args){
-        System.out.println("Hello World ");
-        System.out.println("Sare Jahan Se Achacha Hindustan Hamara ");
-    
-    }
-}
---
+++ Program001.java
@@ -0,0 +1,7 @@
+public class Program001{
+    public static void main(String [] args){
+        System.out.println("Hello World, This is Right Time To learn Java ");
+        System.out.println("Hello All ");
+    
+    }
+}
