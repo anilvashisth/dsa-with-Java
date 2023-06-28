7a9b03469681f73fe89b05e7072ca8b7b1244afc
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-28 22:20:58 +0530

    Update Commit for Program 5

--- Program005.java
+++ Program005.java
@@ -1,8 +1,8 @@
-public class Lecture005{
+public class Program005{
     public static void main(String [] args){
         System.out.println("Hello Java World ");
         System.out.println("\nSare Jahan Se Achacha Hindustan Hamara ");
-        System.out.println("\nHello Java Learner, This is Lecture 5 ");
+        System.out.println("\nHello Java Learner, This is Program 5 ");
 
     
     }
