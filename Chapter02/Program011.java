7527120925002d0b0515099681d682448e71fc24
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-03 19:48:02 +0530

    First Commit Program 11

---
+++ Chapter02/Program011.java
@@ -0,0 +1,9 @@
+//Lecture 11
+public class Program011{
+    public static void main(String [] args){
+        System.out.println("Hello Java World ");
+        System.out.println("Sare Jahan Se Achacha Hindustan Hamara ");
+        System.out.println("Hello Java Learner, This is Lecture 11 ");
+    
+    }
+}
