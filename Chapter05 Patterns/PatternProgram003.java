63d722c97b38759cd2139a9cbc530486efbe7e2f
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-05 23:19:06 +0530

    First Commit PatternProgram 3

---
+++ Chapter05 Patterns/PatternProgram003.java
@@ -0,0 +1,21 @@
+//Pattern Program 3
+//package Patterns;
+
+public class PatternProgram003
+{
+    public static void main(String [] args)
+    {
+       // int row=5;
+
+    for(int i=1;i<=5;i++)
+        {
+        for(int j=1;j<=i;j++)
+            {
+            System.out.print("* ");
+            }
+        System.out.println();   
+        }
+        
+    
+    }
+}
