f8bb253537a74e146d95b5124569d79854438e96
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-06 21:49:05 +0530

    First Commit PatternProgram 11

---
+++ Chapter05 Patterns/PatternProgram011.java
@@ -0,0 +1,22 @@
+//Pattern Program 11
+/*
+* 
+* *
+* * *
+* * * *
+* * * * *
+*/
+public class PatternProgram011
+{
+    public static void main(String [] args)
+    {
+    for(int i=1;i<=5;i++)
+        {
+        for(int j=1;j<=i;j++)
+            {
+            System.out.print("* ");
+            }
+        System.out.println();   
+        }
+    }
+}
