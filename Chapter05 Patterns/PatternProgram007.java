f28ec9820e777c3af55224717f30aa4a64ecce20
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-06 21:48:11 +0530

    First Commit PatternProgram 07

---
+++ Chapter05 Patterns/PatternProgram007.java
@@ -0,0 +1,25 @@
+//Pattern Program 7
+/*
+     *
+    *
+   *
+  *
+ *
+*/
+public class PatternProgram007
+{
+    public static void main(String[]args)
+    {
+    int i,j;
+    for(i=1;i<=5;i++)
+        {
+        for(j=0;j<=5-i;j++)
+        {
+            System.out.print(" ");
+        }
+        System.out.print("*\n");
+        }
+        
+    }
+    
+}
