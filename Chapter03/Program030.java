e4076999b85b954024b03e0e4be7761afa35e4a9
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-06 21:39:17 +0530

    First Commit Program 30

---
+++ Chapter03/Program030.java
@@ -0,0 +1,9 @@
+//Lecture 30
+public class Program030{
+    public static void main(String [] args){
+        System.out.println("Hello Java World ");
+        System.out.println("Sare Jahan Se Achacha Hindustan Hamara ");
+        System.out.println("Hello Java Learner, This is Program 30 ");
+    
+    }
+}
