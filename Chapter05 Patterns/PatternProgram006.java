cf5b7a5cac286751226b72b5124860e5493aab6a
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-06 21:47:14 +0530

    First Commit PatternProgram 06

---
+++ Chapter05 Patterns/PatternProgram006.java
@@ -0,0 +1,22 @@
+//Pattern Program 6
+/*
+54321
+54321
+54321
+54321
+54321
+*/
+public class PatternProgram006
+{
+    public static void main(String [] args)
+    {
+    for(int i=5;i>=1;i--)
+        {
+        for(int j=5;j>=1;j--)
+            {
+            System.out.print(j );
+        }
+        System.out.println();   
+        }
+    }
+}
