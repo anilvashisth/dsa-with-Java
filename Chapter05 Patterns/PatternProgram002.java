72ae2e3737d4b98342c3d3ad22342260a45ec9a7
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-06 21:43:36 +0530

    Updated Commit PatternProgram 02

--- Chapter05 Patterns/PatternProgram002.java
+++ Chapter05 Patterns/PatternProgram002.java
@@ -1,20 +1,22 @@
-//Pattern Program 1
-//package Patterns;
-
-public class PatternProgram001
+//Pattern Program 2
+/*
+12345
+12345
+12345
+12345
+12345
+*/
+public class PatternProgram002
 {
     public static void main(String [] args)
     {
-        
     for(int i=1;i<=5;i++)
         {
         for(int j=1;j<=5;j++)
             {
-            System.out.print(i);
-            }
+            System.out.print(j);
+        }
         System.out.println();   
         }
-        
-    
     }
 }
