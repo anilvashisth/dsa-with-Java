4ce5268f3469093a94c91f33ab85a99e077db8cf
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-07 18:10:30 +0530

    Updated Commit Program 11

--- Chapter02/Program011.java
+++ Chapter02/Program011.java
@@ -2,8 +2,7 @@
 public class Program011{
     public static void main(String [] args){
         System.out.println("Hello Java World ");
-        System.out.println("Sare Jahan Se Achacha Hindustan Hamara ");
-        System.out.println("Hello Java Learner, This is Lecture 11 ");
+        System.out.println("Hello Java Learner, This is Program 11 ");
     
     }
 }
