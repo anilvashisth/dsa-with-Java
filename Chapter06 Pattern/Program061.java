f624f714009fe645f9af2fc8dcf8a29e5130bb2b
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-08 15:32:17 +0530

    Create First Commit Program 61

---
+++ Chapter06 Pattern/Program061.java
@@ -0,0 +1,22 @@
+//Pattern Program 61
+/*
+M M M M M 
+M M M M 
+M M M 
+M M 
+M 
+*/
+public class Program061
+{
+    public static void main(String [] args)
+    {
+    for(int i=5;i>=1;i--)
+        {
+        for(int j=1;j<=i;j++)
+            {
+            System.out.print("M ");
+            }
+        System.out.println();   
+        }   
+    }
+}
