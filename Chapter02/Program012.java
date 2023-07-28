4536906d5a5b4efb267ed7331a60fad8f7b3a492
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-28 20:43:37 +0530

    First Commit Program 12

---
+++ Chapter02/Program012.java
@@ -0,0 +1,8 @@
+//Lecture 12
+public class Program011{
+    public static void main(String [] args){
+        System.out.println("Hello Java World ");
+        System.out.println("Hello Java Learner, This is Program 12 ");
+    
+    }
+}
