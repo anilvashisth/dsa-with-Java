d6b9266beefe7d5cf37a388c0c16a70cf1568265
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-28 20:44:44 +0530

    First Commit Program 31

---
+++ Chapter03/Program031.java
@@ -0,0 +1,8 @@
+//Lecture 31
+public class Program011{
+    public static void main(String [] args){
+        System.out.println("Hello Java World ");
+        System.out.println("Hello Java Learner, This is Program 31 ");
+    
+    }
+}
